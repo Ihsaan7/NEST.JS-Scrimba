90b9a3dd2a25bfa0d46aa395c9aea721880119cc
Ihsaan7 <ihsaan2215@gmail.com>
2026-09-22 21:51:03 +0500

    GET/Post Services added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,62 +1,30 @@
 import { BadRequestException, Controller , Get , NotFoundException, Param, Post , Body , Put , Delete} from '@nestjs/common';
+import { ProductsService } from './products.service.js';
 
 @Controller('products')
 export class ProductsController {
-   private products = [
-        { id: 1, title: "Wireless Mouse", price: 29.99 },
-        { id: 2, title: "Mechanical Keyboard", price: 89.99 },
-        { id: 3, title: "USB-C Hub", price: 49.99 },
-    ];
+    constructor(private readonly productServices : ProductsService ){}
 
-    // GET '/products'
+    // GET /products
     @Get()
     findAll()
     {
-        return{
-            success: true,
-            count:this.products.length,
-            data:this.products
-        }
+        return this.productServices.findAll();
     }
-
-    // GET '/products/:id'
+   
+    // GET /products/:id
     @Get(':id')
     findOne(@Param('id') id:string)
     {
-        const productId = parseInt(id, 10)
-        const product = this.products.find((p => p.id === productId))
-
-        if(!product){
-            throw new NotFoundException(`Product with ID ${id} not found!`)
-        }
-
-        return{
-            success: true,
-            data: product
-        }
+        return this.productServices.findOne(id)
     }
+
 
     // POST '/products'
     @Post()
-    create(@Body() body: {title: string; price: number})
+    create(@Body() body:{title: string , price: number})
     {
-        if(!body.title || body.price === undefined)
-            {
-                throw new BadRequestException('Title and Price are required!')
-            }
-        const newProduct=
-        {
-            id: this.products.length + 1,
-            title: body.title,
-            price: body.price
-        }
-
-        this.products.push(newProduct)
-        return{
-            success:true,
-            message:"Product created successfully",
-            data: newProduct
-        }
+        return this.productServices.create(body)
     }
 
     // PUT '/products/:id'
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+
+      private products = [
+        { id: 1, title: "Wireless Mouse", price: 29.99 },
+        { id: 2, title: "Mechanical Keyboard", price: 89.99 },
+        { id: 3, title: "USB-C Hub", price: 49.99 },
+    ];
+
+    findAll(){
+        return {
+            success: true,
+            count:this.products.length,
+            data:this.products
+        }
+    }
+
+    findOne(id: string)
+    {
+        const productId = parseInt(id , 10)
+        const product = this.products.find(p=> p.id === productId)
+
+        if(!product){ throw new NotFoundException("No product found")}
+        return{
+            success: true,
+            data: product
+        }
+    }
+
+
+    create(body:{title:string; price:number})
+    {
+        if(!body.title || body.price === undefined)
+            {
+                throw new BadRequestException("Both fields requied!")
+            }
+        const newProduct=
+        {
+            id: this.products.length + 1,
+            title: body.title,
+            price: body.price
+        }
+
+        this.products.push(newProduct)
+        return {
+            success: true,
+            message:"Product created",
+            data: newProduct
+        }
+    }
+
+    update(id:string, body:{title?: string , price?:number})
+    {
+        const productId = parseInt(id , 10)
+        
+    }
+}
